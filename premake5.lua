b4f743eb51d61d847445358c30f1d36d24a263ae
OfficialChrisHusky <81988470+OfficialChrisHusky@users.noreply.github.com>
2022-11-02 17:02:51 +0100

    Project Creation

---
+++ premake5.lua
@@ -0,0 +1,23 @@
+workspace "ECS"
+    architecture "x64"
+    configurations { "Debug", "Release" }
+
+outputDir = "%{cfg.buildcfg}-%{cfg.architecture}"
+
+project "ECS"
+    location ""
+    kind "ConsoleApp"
+    language "C++"  
+    cppdialect "C++17"
+
+    targetdir("Build/" .. outputDir)
+    objdir("BuildInt/" .. outputDir)
+
+    files {
+
+        "src/**.h",
+        "src/**.cpp"
+
+    }
+
+    includedirs "src"
